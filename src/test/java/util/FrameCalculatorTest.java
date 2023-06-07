654ec81c718d2dd98fec47374a7118370da05813
dkreth <dylankreth@gmail.com>
2023-06-06 19:10:57 -0700

    first pass of test cases

--- src/test/java/util/FrameCalculatorTest.java
+++ src/test/java/util/FrameCalculatorTest.java
@@ -3,8 +3,6 @@ package util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FrameCalculatorTest {
 
     @Test
@@ -12,5 +10,46 @@ class FrameCalculatorTest {
         String[] result = FrameCalculator.calculate(new String[]{"7", "2", "5", "3", "6", "/", "7", "1", "X", "2", "4"});
         String[] expected = new String[]{"9", "8", "17", "8", "16", "6"};
         Assertions.assertArrayEquals(expected,result);
+    }
+
+    @Test
+    void calculate_givenNonnumericalRoll_shouldThrowException() {
+        
+    }
+
+    @Test
+    void calculate_givenTwoRollsThatSumToMoreThanTen_shouldThrowException() {
+
+    }
+
+
+    @Test
+    void calculate_givenIncompleteStrikeFrame_shouldReturnNullFinalTwoFrames() {
+
+    }
+
+    @Test
+    void calculate_givenIncompleteSpareFrame_shouldReturnNullFinalFrame() {
+
+    }
+
+    @Test
+    void calculate_givenIncompleteNumericalFrame_shouldReturnNullFinalFrame() {
+
+    }
+
+    @Test
+    void calculate_givenSpareAsFirstBall_shouldThrowException() {
+
+    }
+
+    @Test
+    void calculate_givenStrikeAsSecondBall_shouldThrowException() {
+
+    }
+
+    @Test
+    void calculate_givenPerfectGame_totalScoreShouldEqual300() {
+
     }
 }
