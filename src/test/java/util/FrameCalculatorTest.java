c7e09f756828862a881ec6e35e77d825157d7933
dkreth <dylankreth@gmail.com>
2023-06-07 09:42:02 -0700

    implement tests

--- src/test/java/util/FrameCalculatorTest.java
+++ src/test/java/util/FrameCalculatorTest.java
@@ -3,6 +3,8 @@ package util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 class FrameCalculatorTest {
 
     @Test
@@ -14,42 +16,52 @@ class FrameCalculatorTest {
 
     @Test
     void calculate_givenNonnumericalRoll_shouldThrowException() {
-        
+        Assertions.assertThrows(Exception.class, () -> FrameCalculator.calculate(new String[] {"7","2","a","1"}));
     }
 
     @Test
     void calculate_givenTwoRollsThatSumToMoreThanTen_shouldThrowException() {
-
+        Assertions.assertThrows(Exception.class, () -> FrameCalculator.calculate(new String[] {"7","4"}));
     }
-
 
     @Test
     void calculate_givenIncompleteStrikeFrame_shouldReturnNullFinalTwoFrames() {
-
+        String[] result1 = FrameCalculator.calculate(new String[]{"7","2","X"});
+        String[] result2 = FrameCalculator.calculate(new String[]{"7","2","X", "3"});
+        String[] expected1 = new String[] {"9", null};
+        String[] expected2 = new String[] {"9", null, null};
+        Assertions.assertArrayEquals(expected1, result1);
+        Assertions.assertArrayEquals(expected2, result2);
     }
 
     @Test
     void calculate_givenIncompleteSpareFrame_shouldReturnNullFinalFrame() {
-
+        String[] result = FrameCalculator.calculate(new String[]{"7","2","5", "/"});
+        String[] expected = new String[] {"9", null};
+        Assertions.assertArrayEquals(expected, result);
     }
 
     @Test
     void calculate_givenIncompleteNumericalFrame_shouldReturnNullFinalFrame() {
-
+        String[] result = FrameCalculator.calculate(new String[]{"7","2","5"});
+        String[] expected = new String[] {"9", null};
+        Assertions.assertArrayEquals(expected, result);
     }
 
     @Test
     void calculate_givenSpareAsFirstBall_shouldThrowException() {
-
+        Assertions.assertThrows(Exception.class, () -> FrameCalculator.calculate(new String[] {"7","3","/"}));
     }
 
     @Test
     void calculate_givenStrikeAsSecondBall_shouldThrowException() {
-
+        Assertions.assertThrows(Exception.class, () -> FrameCalculator.calculate(new String[] {"7","3", "6","X"}));
     }
 
     @Test
     void calculate_givenPerfectGame_totalScoreShouldEqual300() {
-
+        String[] result = FrameCalculator.calculate(new String[]{"X","X","X","X","X","X","X","X","X","X","X","X",});
+        Integer sum = Arrays.stream(result).map(Integer::parseInt).reduce(0, (runningTotal, nextNum) -> runningTotal + nextNum);
+        Assertions.assertEquals(300, sum);
     }
 }
