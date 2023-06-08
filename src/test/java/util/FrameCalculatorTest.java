051339a8b752dcd1d0a55292e60285bd60686c0f
dkreth <dylankreth@gmail.com>
2023-06-07 23:28:04 -0700

    additional tests for corner cases

--- src/test/java/util/FrameCalculatorTest.java
+++ src/test/java/util/FrameCalculatorTest.java
@@ -61,11 +61,18 @@ class FrameCalculatorTest {
         Assertions.assertThrows(Exception.class, () -> frameCalculator.calculate(new String[] {"7","3", "6","X"}));
     }
 
-    // weird cases
+    // corner cases
     @Test
     void calculate_givenPerfectGame_totalScoreShouldEqual300() {
         String[] result = frameCalculator.calculate(new String[]{"X","X","X","X","X","X","X","X","X","X","X","X",});
         String[] expected = new String[]{"30","30","30","30","30","30","30","30","30","30",null, null};
+        int resultSum = 0;
+        for(String frameScore : result) {
+            if(frameScore != null){
+                resultSum += Integer.parseInt(frameScore);
+            }
+        }
+        Assertions.assertEquals(300, resultSum);
         Assertions.assertArrayEquals(expected, result);
     }
 
@@ -75,4 +82,107 @@ class FrameCalculatorTest {
         String[] expected = new String[]{"20","14","9"};
         Assertions.assertArrayEquals(expected, result);
     }
+
+    @Test
+    void calculate_numbersFollowedByNumbers() {
+        String[] result = frameCalculator.calculate(new String[]{"7", "2", "6", "3"});
+        String[] expected = new String[]{"9","9"};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_numbersFollowedByStrike() {
+        String[] result = frameCalculator.calculate(new String[]{"7", "2", "X"});
+        String[] expected = new String[]{"9",null};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_spareFollowedByNumbers() {
+        String[] result = frameCalculator.calculate(new String[]{"7", "/", "6","3"});
+        String[] expected = new String[]{"16","9"};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_spareFollowedByStrike() {
+        String[] result = frameCalculator.calculate(new String[]{"7", "/", "X"});
+        String[] expected = new String[]{"20",null};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_strikeFollowedByNumbers() {
+        String[] result = frameCalculator.calculate(new String[]{"X", "7", "2"});
+        String[] expected = new String[]{"19","9"};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_strikeFollowedBySoloNumber() {
+        String[] result = frameCalculator.calculate(new String[]{"X", "7"});
+        String[] expected = new String[]{null, null};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_strikeFollowedBySpare() {
+        String[] result = frameCalculator.calculate(new String[]{"X", "7", "/"});
+        String[] expected = new String[]{"20",null};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_strikeFollowedByStrikeFollowedByNumber() {
+        String[] result = frameCalculator.calculate(new String[]{"X", "X", "7"});
+        String[] expected = new String[]{"27",null,null};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_strikeFollowedByStrikeFollowedByStrike() {
+        String[] result = frameCalculator.calculate(new String[]{"X", "X", "X"});
+        String[] expected = new String[]{"30",null,null};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_strikeFollowedByStrike() {
+        String[] result = frameCalculator.calculate(new String[]{"X", "X"});
+        String[] expected = new String[]{null,null};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_soloSpare() {
+        String[] result = frameCalculator.calculate(new String[]{"7", "/"});
+        String[] expected = new String[]{null};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_soloStrike() {
+        String[] result = frameCalculator.calculate(new String[]{"X"});
+        String[] expected = new String[]{null};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_soloNumber() {
+        String[] result = frameCalculator.calculate(new String[]{"7"});
+        String[] expected = new String[]{null};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void calculate_emptyInput() {
+        String[] result = frameCalculator.calculate(new String[]{});
+        String[] expected = new String[]{};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+
+
+
+
 }
