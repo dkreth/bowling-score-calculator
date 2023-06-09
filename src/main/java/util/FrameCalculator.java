1e52f3f082868d7fca829e8fc4dce09d3270a190
dkreth <dylankreth@gmail.com>
2023-06-08 19:24:52 -0700

    refactor: improve readability using helper methods

--- src/main/java/util/FrameCalculator.java
+++ src/main/java/util/FrameCalculator.java
@@ -17,65 +17,82 @@ public class FrameCalculator {
         ListIterator<Roll> iterator = rolls.listIterator();
         while (iterator.hasNext()) {
             Roll currRoll = iterator.next();
-
-            if(currRoll.isNumeric()) {
-                if(iterator.hasNext()) {
-                    Roll nextRoll = iterator.next();
-                    // nextRoll can either be a number or a spare (/)
-                    if(nextRoll.isNumeric()){
-                        results.add(String.valueOf(currRoll.getValue()+nextRoll.getValue()));
-                        continue;
-                    } else {
-                        // if nextRoll is a spare, rollAfterNext can either be a number, strike, or not exist
-                        if(iterator.hasNext()) {
-                            Roll rollAfterNext = iterator.next();
-                            // whether rollAfterNext is a number or a strike, we can just add its value to 10 (10 is for the base score of the spare frame)
-                            results.add(String.valueOf(10 + rollAfterNext.getValue()));
-                            // unwinding the iterator.next() we did to peek rollAfterNext's value
-                            iterator.previous();
-                        } else {
-                            // incomplete frame because nextRoll is a spare and there is no follow-up roll
-                            results.add(null);
-                        }
-                    }
-                    // no need to unwind the iterator.next() we did for to get nextRoll since we finished the frame
-                } else {
-                    // incomplete numeric frame
-                    results.add(null);
-                }
-            } else {
-                // didn't start with a number, so it must be a strike
-                // strike is 10 + nextRoll + rollAfterNext
-                if(iterator.hasNext()) {
-                    Roll nextRoll = iterator.next();
-                    // nextRoll can either be a strike, a number, or not exist
-                    if(iterator.hasNext()) {
-                        Roll rollAfterNext = iterator.next();
-                        // nextRoll-rollAfterNext pairing can either be: number-number, number-/, X-number, X-X
-
-                        if(nextRoll.isNumeric() && rollAfterNext.equals(Roll.SPARE)){
-                            // in case of number-/, (nextRoll + rollAfterNext) == 10
-                            results.add(String.valueOf(10+10));
-                        } else {
-                            // in case of number-number, X-number, or X-X, we can just add the two values (using 10 for the value of X)
-                            results.add(String.valueOf(10 + nextRoll.getValue() + rollAfterNext.getValue()));
-                        }
-
-                        // unwinding the iterator.next() we did to peek rollAfterNext's value
-                        iterator.previous();
-                    } else {
-                        // strike followed by roll followed by nothing
-                        results.add(null);
-                    }
-                    // unwinding the iterator.next() we did to peek nextRoll's value
-                    iterator.previous();
-                } else {
-                    // strike followed by nothing
-                    results.add(null);
-                }
-            }
+            String frameResult = calculateFrameResult(iterator, currRoll);
+            results.add(frameResult);
         }
 
         return results.toArray(new String[0]);
+    }
+
+    private static String calculateFrameResult(ListIterator<Roll> iterator, Roll currRoll) {
+        String frameResult;
+        if(currRoll.isNumeric()) {
+            frameResult = handleNumericFrame(iterator, currRoll);
+        } else {
+            // didn't start with a number, so it must be a strike
+            frameResult = handleStrikeFrame(iterator);
+        }
+        return frameResult;
+    }
+
+    private static String handleNumericFrame(ListIterator<Roll> iterator, Roll currRoll) {
+        String frameResult;
+        if(iterator.hasNext()) {
+            Roll nextRoll = iterator.next();
+            // nextRoll can either be a number or a spare (/)
+            if(nextRoll.isNumeric()){
+                frameResult = String.valueOf(currRoll.getValue()+nextRoll.getValue());
+            } else {
+                // if nextRoll is a spare, rollAfterNext can either be a number, strike, or not exist
+                if(iterator.hasNext()) {
+                    Roll rollAfterNext = iterator.next();
+                    // whether rollAfterNext is a number or a strike, we can just add its value to 10 (10 is for the base score of the spare frame)
+                    frameResult = String.valueOf(10 + rollAfterNext.getValue());
+                    // unwinding the iterator.next() we did to peek rollAfterNext's value
+                    iterator.previous();
+                } else {
+                    // incomplete frame because nextRoll is a spare and there is no follow-up roll
+                    frameResult = null;
+                }
+            }
+            // no need to unwind the iterator.next() we did for to get nextRoll since we finished the frame
+        } else {
+            // incomplete numeric frame
+            frameResult = null;
+        }
+        return frameResult;
+    }
+
+    private static String handleStrikeFrame(ListIterator<Roll> iterator) {
+        String frameResult;
+        // strike is 10 + nextRoll + rollAfterNext
+        if(iterator.hasNext()) {
+            Roll nextRoll = iterator.next();
+            // nextRoll can either be a strike, a number, or not exist
+            if(iterator.hasNext()) {
+                Roll rollAfterNext = iterator.next();
+                // nextRoll-rollAfterNext pairing can either be: number-number, number-/, X-number, X-X
+
+                if(nextRoll.isNumeric() && rollAfterNext.equals(Roll.SPARE)){
+                    // in case of number-/, (nextRoll + rollAfterNext) == 10
+                    frameResult = String.valueOf(10+10);
+                } else {
+                    // in case of number-number, X-number, or X-X, we can just add the two values (using 10 for the value of X)
+                    frameResult = String.valueOf(10 + nextRoll.getValue() + rollAfterNext.getValue());
+                }
+
+                // unwinding the iterator.next() we did to peek rollAfterNext's value
+                iterator.previous();
+            } else {
+                // strike followed by roll followed by nothing
+                frameResult = null;
+            }
+            // unwinding the iterator.next() we did to peek nextRoll's value
+            iterator.previous();
+        } else {
+            // strike followed by nothing
+            frameResult = null;
+        }
+        return frameResult;
     }
 }
